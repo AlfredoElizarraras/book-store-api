40bece1f2d5398c6f2cf1d692bc8506671432916
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-28 11:06:27 -0600

    Create routes for retrieving and updating user information.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions },
                        path_names: { sign_in: :login }
+              
+    resources :user, only: [:update, :show]
   end
 end
