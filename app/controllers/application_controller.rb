c8abc7a54f36260c6eec19af244533c72637ade9
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-28 08:47:23 -0600

    Create a filter to convert incoming parameters to snake_case.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  before_action :underscore_params!
+
+  private
+
+  def underscore_params!
+    deep_transform_keys(params, &:underscore)
+  end
+
+  # https://apidock.com/rails/v4.0.2/Hash/deep_transform_keys
+  def deep_transform_keys(value, &block)
+    result = {}
+    value.each do |key, value|
+      result[yield(key)] = value.is_a?(Hash) ? deep_transform_keys(value, &block) : value
+    end
+    result
+  end
 end
